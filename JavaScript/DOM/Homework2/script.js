177d5e5e17d5ffd75fd63011b603ac139a93208a
dovilemik <dovile.mikenaite@gmail.com>
2025-03-05 19:53:33 +0200

    DOM Homework #2

---
+++ JavaScript/DOM/Homework2/script.js
@@ -0,0 +1,23 @@
+const body = document.getElementById('body');
+
+const firstParagraph = document.getElementById("first");
+const secondParagraph = document.getElementById("second");
+const thirdParagraph = document.getElementById("third");
+
+const newfirstParagraph = document.createElement('p');
+newfirstParagraph.innerText = 'Naujas pirmas paragrafas';
+newfirstParagraph.style.color = 'coral';
+newfirstParagraph.style.fontSize = '20px';
+newfirstParagraph.style.fontFamily= 'cursive';
+
+const newsecondParagraph = document.createElement('h2');
+newsecondParagraph.innerText = 'Naujas antras paragrafas - H2';
+newsecondParagraph.style.color = 'red'
+
+const newthirdParagraph = document.createElement('div');
+newthirdParagraph.innerText = 'Naujas trečias paragrafas - div';
+newthirdParagraph.style.fontStyle = 'italic';
+
+body.replaceChild(newfirstParagraph, firstParagraph);
+body.replaceChild(newsecondParagraph, secondParagraph);
+body.replaceChild(newthirdParagraph, thirdParagraph);
