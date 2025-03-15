8b405f70cf04d1c96600487b11d13c88e5296306
dovilemik <dovile.mikenaite@gmail.com>
2025-03-15 12:10:57 +0200

    update

--- JavaScript/Ciklai/ciklai.js
+++ JavaScript/Ciklai/ciklai.js
@@ -17,17 +17,17 @@
 
 const fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
 
-for(let i = 0; i <fruits.length; i++) {
+for (let i = 0; i < fruits.length; i++) {
     console.log(fruits[i])
 }
 
 // Užduotis 2 - Sumos skaičiavimas
 
-const numbers = [1,3,7,8,2,10,4,5,6,9];
+const numbers = [1, 3, 7, 8, 2, 10, 4, 5, 6, 9];
 let sum = 0;
 
-for(let i = 0; i < numbers.length; i++) {
-    if(numbers[i] > 5) {
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 5) {
         sum = sum + numbers[i];
     }
 }
@@ -36,19 +36,19 @@ console.log('Total sum of numbers greater than 5: ' + sum);
 
 // Užduotis 3 - Balsavimo tinkamumo nustatymas
 
-const people = [ 
-	{ name: 'Alice', age: 25, validResidence: true },
-	{ name: 'Bob', age: 17, validResidence: true },
-	{ name: 'Charlie', age: 30, validResidence: false },
-	{ name: 'John', age: 13, validResidence: true },
-	{ name: 'Amy', age: 24, validResidence: true }
+const people = [
+    { name: 'Alice', age: 25, validResidence: true },
+    { name: 'Bob', age: 17, validResidence: true },
+    { name: 'Charlie', age: 30, validResidence: false },
+    { name: 'John', age: 13, validResidence: true },
+    { name: 'Amy', age: 24, validResidence: true }
 ];
 
 const canVote = [];
 const cantVote = [];
 
-for(i=0; i < people.length; i++) {
-    if (people[i].age > 18 && people[i].validResidence === true){
+for (i = 0; i < people.length; i++) {
+    if (people[i].age > 18 && people[i].validResidence === true) {
         canVote.push(people[i])
     } else {
         cantVote.push(people[i])
@@ -56,11 +56,11 @@ for(i=0; i < people.length; i++) {
 }
 
 console.log('People who can vote')
-for(i=0; i < canVote.length; i++) {
+for (i = 0; i < canVote.length; i++) {
     console.log(canVote[i].name)
 }
 
 console.log("People who can't vote")
-for(i=0; i < cantVote.length; i++) {
+for (i = 0; i < cantVote.length; i++) {
     console.log(cantVote[i].name)
 }
